f032ed66df9552b56742162f7894652a8d31f837
Akarsh <akarshtripathi02@gmail.com>
2024-06-03 12:51:07 +0530

    Project Initilizaion + Database Model Declaration with Serializers

---
+++ IdentityConsole/IdentityAPP/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+
+class ContactModel(models.Model):
+    id = models.AutoField(primary_key=True, increament=True)
+    phonenumber = models.IntegerField(null=True, min_length=10, max_length=10)
+    email = models.EmailField()
+    linkedId = models.IntegerField(null=True)
+    linkPrecedence = models.CharField(max_length=10)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+    deletedAt = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.name
+    
+
+
+# {
+# id Int 
+#  phoneNumber String?
+#  email String?
+#  linkedId Int? // the ID of another Contact linked to this one
+#  linkPrecedence "secondary"|"primary" // "primary" if it's the first Contact in th
+#  createdAt DateTime 
+# Bitespeed Backend Task: Identity Reconciliation 2
+#  updatedAt DateTime 
+#  deletedAt DateTime?
+# }
